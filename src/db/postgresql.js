68a6bbc25f1302e46a8d37e39cbe52a3620ca23b
Nicolas <nicojs2011@gmail.com>
2024-08-05 17:48:45 -0400

    msj de comprobacion de conexcion de bd al iniciar api

--- src/db/postgresql.js
+++ src/db/postgresql.js
@@ -1,15 +1,30 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 const query = (text, params, callback) => {
-    return pool.query(text, params, callback)
+  return pool.query(text, params, callback);
+};
+
+const verification = async () => {
+  try {
+    const res = await pool.query("SELECT $1::text as message", [
+      "Connected to the database!",
+    ])
+    console.log(res.rows[0].message) // Hello world!
+  } catch (err) {
+    console.error(err)
+    process.exit(1);
+  }
 }
 
-module.exports = {pool,query}
+module.exports = {
+  query,
+  verification,
+};
--- src/main.js
+++ src/main.js
@@ -1,28 +1,29 @@
-const fastify = require('fastify')(
-    {logger: false}
-)
+const fastify = require("fastify")({ logger: true });
+const { verification } = require("./db/postgresql");
+const routeLicense = require("./routes/routes.js");
 
-const routeLicense = require('./routes/routes.js')
+// Database connection verification
+verification()
 
-// Declare a route
-fastify.get('/', (request, reply) => {
-    reply.send({ msg:'Wellcome'})
-})
+// Wellcome route
+fastify.get("/", (request, reply) => {
+  reply.send({ msg: "Wellcome" });
+});
 
-routeLicense.forEach(route => {
-    fastify.route(route)
-})
+routeLicense.forEach((route) => {
+  fastify.route(route);
+});
 
 const start = async () => {
-    try {
-        // Start the server on port 3000, listening on all network interfaces
-        await fastify.listen({ port: 3000, host: '0.0.0.0' })
-        // Log a message to indicate that the API is online
-        console.log(`Api en linea`)
-    } catch (err) {
-        // Log any error that occurs during server startup and exit the process
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+  try {
+    // Start the server on port 3000, listening on all network interfaces
+    await fastify.listen({ port: 3000, host: "localhost" });
+    // Log a message to indicate that the API is online
+    console.log(`Api en linea`);
+  } catch (err) {
+    // Log any error that occurs during server startup and exit the process
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
